9ee14917e896caf8985d48b39391c184c532ef8f
Meenakshi Nadimuthu <46770425+meenukrish@users.noreply.github.com>
2019-07-01 02:46:55 -0700

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -33,22 +33,32 @@ function submitted()
 {
   
 var billamount = d3.select("#billtotal").node().value;
-var tippercent = d3.select("#tippercent").node().value;
-
-tippercentval = tippercent.split("%");
-
-var tipvalue = ((billamount * tippercentval[0])/100).toFixed(2)
-
-d3.select("#totaltip").text(`$${tipvalue}`)
-
-
-if (includepeople =="yes")
+var tipvalue = 0;
+if(billamount > 0 )
 {
-  nofpeople = d3.select("#nofpeople").node().value; 
-  var tipperson= (tipvalue/nofpeople).toFixed(2); 
-  d3.select("#tipperperson").text(`$${tipperson}`)
 
-}
+    var tippercent = d3.select("#tippercent").node().value;
+
+    tippercentval = tippercent.split("%");
+
+    tipvalue = ((billamount * tippercentval[0])/100).toFixed(2)
+
+    d3.select("#totaltip").text(`$${tipvalue}`)
+
+    if (includepeople =="yes")
+    {
+      nofpeople = d3.select("#nofpeople").node().value; 
+      var tipperson= (tipvalue/nofpeople).toFixed(2); 
+      d3.select("#tipperperson").text(`$${tipperson}`)
+
+    }
+
+  }
+  else
+  {
+    d3.select("#totaltip").text(`$${tipvalue}`)
+    d3.select("#tipperperson").text(`$${tipvalue}`)
+  }
 
 
 };
